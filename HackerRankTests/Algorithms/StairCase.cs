a6fd6a8f9575bdb2e2d13f5647decec0f321eb19
Paige Watson <paige.watson@outlook.com>
2017-05-17 11:11:05 -0700

    Added Staircase Test

---
+++ HackerRankTests/Algorithms/StairCase.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HackerRankTests.Algorithms
+{
+    [TestClass]
+    public class StairCase
+    {
+        [TestMethod, TestCategory("Unit")]
+        public void ShouldWriteOutOneLevel()
+        {
+            //assign
+            int inputNumber = 1;
+            //act
+            string output = GetStaircase(inputNumber);
+            //assert
+            output.Should().Be("#");
+        }
+        [TestMethod, TestCategory("Unit")]
+        public void ShouldWriteOutTwoLevels()
+        {
+            //assign
+            int inputNumber = 2;
+            //act
+            string output = GetStaircase(inputNumber);
+            //assert
+            output.Should().Be(" #\r\n##");
+        }
+        [TestMethod, TestCategory("Unit")]
+        public void ShouldWriteOutFourLevels()
+        {
+            //assign
+            int inputNumber = 4;
+            //act
+            string output = GetStaircase(inputNumber);
+            //assert
+            output.Should().Be("   #\r\n  ##\r\n ###\r\n####");
+        }
+
+        private string GetStaircase(int inputNumber)
+        {
+            string output = String.Empty;
+            for (int i = 0; i < inputNumber; i++)
+            {
+                output += $"{"".PadLeft(i + 1, '#').PadLeft(inputNumber, ' ')}{(i < inputNumber - 1 ? "\r\n" : string.Empty)}";
+            }
+            return output;
+        }
+    }
+}
