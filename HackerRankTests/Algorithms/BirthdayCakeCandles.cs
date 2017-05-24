a61226cd65f63f60a271c9609376d7cbce136afe
Paige Watson <paige.watson@outlook.com>
2017-05-23 20:07:58 -0700

    Added two more to warm ups

---
+++ HackerRankTests/Algorithms/BirthdayCakeCandles.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HackerRankTests.Algorithms
+{
+    [TestClass]
+    public class BirthdayCakeCandles
+    {
+        [TestMethod, TestCategory("Unit")]
+        public void ShouldReturnHighestCandles()
+        {
+            //assign
+            int[] inputArr = new[] { 3, 2, 1, 3 };
+            //act
+            int highest = GetHighest(inputArr);
+            //assert
+            highest.Should().Be(3);
+
+        }
+        [TestMethod, TestCategory("Unit")]
+        public void ShouldCountHighestCandles()
+        {
+            //assign
+            int[] inputArr = new[] { 3, 2, 1, 3 };
+            //act
+            int highest = CountHighest(inputArr);
+            //assert
+            highest.Should().Be(2);
+
+        }
+        
+        private static int CountHighest(int[] inputArr)=>inputArr.Count(candle => candle == GetHighest(inputArr));
+        private static int GetHighest(int[] inputArr)=>inputArr.Max();
+    }
+}
---
+++ HackerRankTests/Algorithms/TimeConversion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HackerRankTests.Algorithms
+{
+    [TestClass]
+    public class TimeConversion
+    {
+        [TestMethod, TestCategory("Unit")]
+        public void ShouldConvert12HourTimeTo24HourTime()
+        {
+            //assign
+            string startTime = "07:05:45PM";
+
+            //act
+            DateTime timeObj = DateTime.Parse(startTime);
+            //assert
+            DateTime.Parse(startTime).ToString("HH:mm:ss").Should().Be("19:05:45");
+        }
+
+    }
+}
