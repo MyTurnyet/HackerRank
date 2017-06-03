3f92724ac3e9b31b7fab4b262344f83549e5b6c8
Paige Watson <paige.watson@outlook.com>
2017-06-02 20:14:41 -0700

    Didgital Difference

---
+++ HackerRankTests/Algorithms/DiagonalDifference.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HackerRankTests.Algorithms
+{
+    [TestClass]
+    public class DiagonalDifference
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        public int main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var suma = 0;
+            int sumb = 0;
+            for (var i = 0; i < n; i++)
+            {
+                var line = Console.ReadLine().Split(' ');
+                suma += int.Parse(line[i]);
+                sumb += int.Parse(line[n - i - 1]);
+            }
+            Console.WriteLine(Math.Abs(sumb - suma));
+        }
+    }
+}
